836a99746e25b917cfb436c3f7f24cd9eef3a577
JoaoTeixeira20 <joao.tex@msn.com>
2020-09-15 13:46:32 +0100

    changed header route links to buttons

--- components/main/Header.js
+++ components/main/Header.js
@@ -2,9 +2,9 @@ import Link from 'next/link'
 
 export default function Header() {
   return <div style={{display:"flex", flexDirection:"row", justifyContent:"center"}}>
-    <Link href="/"><a>index</a></Link> |
-    <Link href="/springcontextexample"><a>spring context</a></Link> |
-    <Link href="/springwithoutcontextexample"><a>mouse handler test</a></Link> |
-    <Link href="/parallax"><a>parallax</a></Link>
+    <Link href="/"><button>index</button></Link> |
+    <Link href="/springcontextexample"><button>2D prespective</button></Link> |
+    <Link href="/springwithoutcontextexample"><button>mouse handler</button></Link> |
+    <Link href="/parallax"><button>parallax</button></Link>
   </div>
 }
