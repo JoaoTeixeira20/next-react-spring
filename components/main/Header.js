94ee745477982710f44cc8c39212ca8c3e3a15c2
JoaoTeixeira20 <joao.tex@msn.com>
2020-09-16 00:39:14 +0100

    added transitions (and oh god, where is that documentation for 9 at?)

--- components/main/Header.js
+++ components/main/Header.js
@@ -1,10 +1,11 @@
 import Link from 'next/link'
 
 export default function Header() {
-  return <div style={{display:"flex", flexDirection:"row", justifyContent:"center"}}>
+  return <div style={{display:"flex", flexDirection:"row", justifyContent:"center", flexWrap: "wrap"}}>
     <Link href="/"><button>index</button></Link> |
     <Link href="/springcontextexample"><button>2D prespective</button></Link> |
     <Link href="/springwithoutcontextexample"><button>mouse handler</button></Link> |
-    <Link href="/parallax"><button>parallax</button></Link>
+    <Link href="/parallax"><button>parallax</button></Link> | 
+    <Link href="/playground"><button>transition</button></Link>
   </div>
 }
---
+++ pages/playground.js
@@ -0,0 +1,39 @@
+import { animated, useTransition, config } from 'react-spring'
+
+const items = ['hello','this','is','flip','magic']
+
+const Item = ({style, value}) => (
+  <animated.div style={{...style,
+    position:"absolute",
+    width:"140px",
+    height:"140px",
+    left:"50%",
+    top:"50%",
+    border:"1px solid black",
+    display:"flex",
+    justifyContent:"center",
+    alignItems:"center"}}><p style={{transform:"rotate(45deg)", fontSize:"3em"}}>{value}</p></animated.div>
+);
+
+export default function Layout(){
+
+  const [index, setIndex] = React.useState(0);
+
+  const next = () => {
+    setIndex(prevState => (prevState + 1) % items.length);
+  };
+
+  const transition = useTransition(items[index], {
+    from: { opacity: 0, transform: 'translate3d(50%,0,0) translate3D(-50%, -50%, 0) rotate(-45deg)' },
+    enter: { opacity: 1, transform: 'translate3d(0%,0,0) translate3D(-50%, -50%, 0) rotate(45deg)' },
+    leave: { opacity: 0, transform: 'translate3d(-50%,0,0) translate3D(-50%, -50%, 0) scale(0deg)' },
+    config: config.stiff
+  });
+
+  return (
+    <div className="App" onClick={next}>
+      {/* {transition(Item)} */}
+      {transition ((style,item) => <Item style={style} value={item}/>)}
+    </div>
+  );
+}
